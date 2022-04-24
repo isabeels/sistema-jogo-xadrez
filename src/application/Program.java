f9363e7f45a8217e417823b2e0e04501aebceda9
isabeels <101607831+isabeels@users.noreply.github.com>
2022-04-24 00:33:35 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Position p1 = new Position(2,4);
+		
+		System.out.println(p1);
+		
 	}
 
 }
